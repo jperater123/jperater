b459251d1e09e4db438f335ed6c523490da7549e
jperater123 <133180223+jperater123@users.noreply.github.com>
2024-06-04 11:47:22 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,6 @@
-// document.getElementById('download_cv').addEventListener('click', function(event) {
-//     event.preventDefault();
-
-//     var file = 'C:\Users\Apet\Desktop\APET WEB PRACTICE\portfolio\files\CV.pdf';
-//     var a = document.createElement('a');
-
-//     a.download = 'CV.pdf';
-//     a.href=file;
-
-//     document.body.appendChild(a);
-//     a.click();
-//     document.body.removeChild(a);
-
-
-// });
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Script loaded'); // Check if the script is loaded
-
-    var downloadLink = document.getElementById('download_cv');
-
-    if (downloadLink) {
-        console.log('Download link found'); // Check if the element with id 'download_cv' is found
-        downloadLink.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevent the default behavior of the anchor element
-
-            var fileUrl = 'files\\CV.pdf';
-            var a = document.createElement('a');
-            a.download = 'your-cv.pdf';
-            a.href = fileUrl;
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
-        });
-    } else {
-        console.log('Download link not found'); // Log an error message if the element is not found
-    }
-});
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
